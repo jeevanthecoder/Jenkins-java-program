d7a892e3c4b85827d4c2741b3840444a272164ed
jeevanthecoder <jeevanharish897@gmail.com>
2024-12-22 11:00:26 +0530

    jenkins first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.sql.Date;
+
+public class Main{
+public static void main(String[] args) {
+    System.out.println("This is a testing program from java!!!");
+    System.out.println(new Date(0).toLocalDate());
+}
+}
