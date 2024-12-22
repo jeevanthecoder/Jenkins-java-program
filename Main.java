204a394dffa5043d1cc704119e1fe5ce5023cb91
jeevanthecoder <jeevanharish897@gmail.com>
2024-12-22 11:07:34 +0530

    second commit

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main{
 public static void main(String[] args) {
     System.out.println("This is a testing program from java!!!");
     System.out.println(new Date(0).toLocalDate());
+    System.out.println("Simple test");
 }
 }
